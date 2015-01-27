2cc03b89fce35e6c48e88b4dfa9e7bc2dbf0007e
dominik2cucu <dominik2cucu@gmail.com>
2015-01-27 18:32:59 +0100

    Move local db config to dev config file.

--- config/dev.exs
+++ config/dev.exs
@@ -10,3 +10,6 @@ config :phoenix, :code_reloader, true
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+
+config :phoenix_crud, PhoenixCrud.Repo,
+  url: "ecto://username:password@localhost/phoenix_crud"
