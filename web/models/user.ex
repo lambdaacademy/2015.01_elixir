399e7447782e0b1b2b68f91f3b0c612c9be18f0d
dominik2cucu <dominik2cucu@gmail.com>
2015-01-30 03:59:12 +0100

    Validate email format.

--- web/models/user.ex
+++ web/models/user.ex
@@ -2,7 +2,7 @@ defmodule PhoenixCrud.User do
   use Ecto.Model
 
   validate user,
-    email: present(),
+    email: has_format(~r/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/),
     password: present(),
     admin: present()
 
