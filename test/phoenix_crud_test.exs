3afd439be1518e9339ce182b0a86fe6b5512b564
Tomasz Kowal <tomasz.kowal@erlang-solutions.com>
2015-01-31 13:54:57 +0100

    Add basic test for homepage using process session

--- test/phoenix_crud_test.exs
+++ test/phoenix_crud_test.exs
@@ -1,7 +1,30 @@
 defmodule PhoenixCrudTest do
   use ExUnit.Case
+  use Plug.Test
 
-  test "the truth" do
-    assert(true)
+  setup_all do
+    session_options = Plug.Session.init(store: Plug.ProcessStore, key: "foobar")
+    {:ok, [session_options: session_options]}
+  end
+
+  def action(controller,
+             verb,
+             action,
+             session_options \\ nil,
+             params \\ nil,
+             headers \\ []) do
+    conn = conn(verb, "/", params, headers)
+    conn = Plug.Session.call(conn, session_options) |> fetch_session
+    controller.call(conn, controller.init(action))
+  end
+
+  test "frontpage should contain word CRUD", context do
+    response = action(PhoenixCrud.WelcomeController,
+                      :get,
+                      :index,
+                      context[:session_options])
+    assert String.contains?(response.resp_body, "CRUD")
+    assert response.state == :sent
+    assert response.status == 200
   end
 end
